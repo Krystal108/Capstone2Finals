f2aef952fc36278ae665f6eed030300410f8af0c
Krystal108 <89510897+Krystal108@users.noreply.github.com>
2024-11-23 19:04:52 +0800

    Update payroll.php

--- public/pages/payroll.php
+++ public/pages/payroll.php
@@ -1,3 +1,17 @@
+<?php
+$host = 'localhost';
+$user = 'root';
+$password = '';
+$database = 'hr_management';
+$port = 3307;
+
+$conn = new mysqli($host, $user, $password, $database, $port);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+?>
+    
 <!DOCTYPE html>
 <html lang="en">
 <head>
